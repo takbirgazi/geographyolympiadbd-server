57824d6911ef0e1186b7b40d16fce1246737ef30
takbirgazi <takbirgazibd@gmail.com>
2024-05-29 16:13:15 +0600

    Basic Setup Complete

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// MongoDB Start
 
-const port = process.env.PORT || 5000;
+// MongoDB End
 
 app.get('/', (req, res)=>{
     res.send(`Welcome to Geography Olympiad BD`);
 })
-
 app.listen(port, ()=>{
     console.log(`Geography Olympiad BD is running at ${port}`);
 })
