2e0e9a077913e1b1b015adf52ae174a4ed260fa9
takbirgazi <takbirgazibd@gmail.com>
2024-06-06 12:40:48 +0600

    setup Done

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // Database and Collections
         const database = client.db('geographyolympiad');
         const allUsers = database.collection('users');
@@ -56,8 +56,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
